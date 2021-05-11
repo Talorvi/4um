93b51e23320674d79a5d0ef54afe29707e0f7474
Mateusz Siedlarz <smateusz96@gmail.com>
2021-05-11 12:07:16 +0200

    added test for GenerateTokenJob

---
+++ tests/Unit/Domains/Authentication/Jobs/GenerateTokenJobTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Domains\Authentication\Jobs;
+
+use Tests\TestCase;
+use App\Domains\Authentication\Jobs\GenerateTokenJob;
+
+class GenerateTokenJobTest extends TestCase
+{
+    public function test_generate_token_job()
+    {
+        $this->markTestIncomplete();
+    }
+}
