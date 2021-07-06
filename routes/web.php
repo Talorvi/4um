34b840f5dd6554cb86068bac1ff22d070023b4f6
Mateusz Siedlarz <smateusz96@gmail.com>
2021-07-06 19:21:47 +0200

    Updated frontend

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/register', function () {
+    return view('welcome');
+});
+Route::get('/login', function () {
+    return view('welcome');
+});
+Route::get('/notifications', function () {
+    return view('welcome');
+});
+Route::get('/favorities', function () {
+    return view('welcome');
+});
+Route::get('/creator', function () {
+    return view('welcome');
+});
+Route::get('/editor', function () {
+    return view('welcome');
+});
+Route::get('/thread', function () {
+    return view('welcome');
+});
+Route::get('/profile', function () {
+    return view('welcome');
+});
+Route::get('/profile/settings', function () {
+    return view('welcome');
+});
+Route::get('/verify', function () {
+    return view('welcome');
+});
